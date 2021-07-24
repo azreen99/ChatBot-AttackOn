5ef0bba6f7ea6f60f0e3afed6a7a75402a48f12f
Muhd_Suhaimii <ghost_bombardment@users.noreply.github.com>
2021-07-25 04:19:30 +0800

    latest system

--- chatbot.py
+++ chatbot.py
@@ -1,4 +1,3 @@
-
 from flask import Flask, render_template, request
 from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
@@ -9,16 +8,49 @@ app = Flask(__name__)
 with open('file.txt', 'r') as file:
     conversation = file.read()
 
-bot = ChatBot("UTP ChatBot", logic_adapters=["chatterbot.logic.MathematicalEvaluation",
-                                             "chatterbot.logic.BestMatch"
-                                             ])
+botname = "PetroBot"
+bot = ChatBot(
+    botname,
+    logic_adapters=["chatterbot.logic.MathematicalEvaluation",
+                    "chatterbot.logic.BestMatch"
+                    ])
+
+weather = [
+    "What's the weather today?",
+    "So hot!!",
+    "Bila nak hujan",
+    "Tahla, esok kot",
+    "Mana masjid?",
+    "Dekat tasik"
+]
+cafe = [
+    "What's the weather today?",
+    "So hot!!",
+    "Bila nak hujan",
+    "Tahla, esok kot",
+    "Mana masjid?",
+    "Dekat tasik"
+]
+sport_facility = [
+    "badminton",
+    "At block A",
+    "swimming",
+    "In front mosque"
+]
+
 trainer = ListTrainer(bot)
-trainer.train(conversation)
+for items in (weather, cafe, sport_facility, conversation):
+    trainer.train(items)
+
+# corpus_trainer = ChatterBotCorpusTrainer(bot)
+# corpus_trainer.train("chatterbot.corpus.english")
+
+trainer.export_for_training('./my_export.json')
 
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("home.html", botname=botname)
 
 
 @app.route("/get")
@@ -28,4 +60,4 @@ def get_bot_response():
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(port=5500)

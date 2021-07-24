a493461576d00482a71df7401b6b38ab9d636116
azreen99 <nurul_17001266@utp.edu.my>
2021-07-25 04:56:55 +0800

    CafeQ azreen

--- chatbot.py
+++ chatbot.py
@@ -23,13 +23,21 @@ weather = [
     "Mana masjid?",
     "Dekat tasik"
 ]
-cafe = [
-    "What's the weather today?",
-    "So hot!!",
-    "Bila nak hujan",
-    "Tahla, esok kot",
-    "Mana masjid?",
-    "Dekat tasik"
+cafeQ = [
+    "How many cafes are there in UTP?",
+    "There are 11 cafes available in UTP such as V1 V2 V3 V4 V5 V6 Cafe Sayang Pocket C Al-Marjan TEEPEE Cafe Pocket D",
+    "Usual operation hours for cafes?",
+    "Most cafes are available starting from 7:00AM till 12:00PM",
+    "V1 cafe",
+    "V1 cafe is located near UTP bus station",
+    "V4 cafe location",
+    "V4 cafe is located near UTP post office station",
+    "V2 cafe menu for today",
+    "There are roti canai, nasi lemak, mee goreng and more!",
+    "The nearest cafe around Block K?",
+    "Nearest cafe around Block K are Cafe Sayang & Al-Marjan",
+    "Cafe that sell pizza?",
+    "V1 cafe are selling various types of pizza Lets give a visit!"
 ]
 sport_facility = [
     "badminton",
@@ -39,7 +47,7 @@ sport_facility = [
 ]
 
 trainer = ListTrainer(bot)
-for items in (weather, cafe, sport_facility, conversation):
+for items in (weather, cafeQ, sport_facility, conversation):
     trainer.train(items)
 
 # corpus_trainer = ChatterBotCorpusTrainer(bot)
